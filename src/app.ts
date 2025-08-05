0c972d84c5b241dde070b832df6650dbfb6ba2e5
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-08-04 23:51:19 -0500

    feat: add swagger documentation support

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { type Request, type Response } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import { swaggerSpec } from './swagger'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Tobimarks API')
---
+++ src/swagger.ts
@@ -0,0 +1,23 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Tobimarks',
+      version: '1.0.0',
+      description: 'API documentation for my application',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ]
+  },
+  
+    apis: [],
+ 
+}
+
+export const swaggerSpec = swaggerJSDoc(options)
