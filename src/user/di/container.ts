c422ee8f6580e06aca14190034018a25743091a9
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-08-12 01:12:12 -0500

    feat: add dependency injection container and tokens for user service

---
+++ src/user/di/container.ts
@@ -0,0 +1,8 @@
+import { container } from 'tsyringe'
+
+import { USER_REPOSITORY, USER_SERVICE } from './tokens'
+import { UserRepository } from '../repositories/user.repository'
+import { UserService } from '../services/user.service'
+
+container.register(USER_SERVICE, { useClass: UserService })
+container.register(USER_REPOSITORY, { useClass: UserRepository })
---
+++ src/user/di/tokens.ts
@@ -0,0 +1,5 @@
+export const USER_SERVICE = Symbol.for('UserService')
+export const USER_CONTROLLER = Symbol.for('UserController')
+export const USER_REPOSITORY = Symbol.for('IUserRepository')
+
+export const TOKEN_SERVICE = Symbol.for('TokenService')
