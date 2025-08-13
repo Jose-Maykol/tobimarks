b05d052142fc9de67aa0044d268707c833eb2593
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-08-12 23:30:33 -0500

    refactor: restructure server startup logic to use async function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import 'dotenv/config'
 import 'reflect-metadata'
 
-import { app } from './app'
 import { configureContainer } from './container'
 import { env } from './core/config/env.config'
 
-configureContainer()
+async function startServer() {
+	configureContainer()
+	const { app } = await import('./app')
+	app.listen(env.PORT, () => {
+		console.log(`🚀 Server running on http://localhost:${env.PORT}`)
+	})
+}
 
-app.listen(env.PORT, () => {
-	console.log(`Server is running in http://localhost:${env.PORT}`)
-})
+startServer().catch(console.error)
