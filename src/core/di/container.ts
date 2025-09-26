337e7667f0125f74d53cb21520c96b485060146d
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-09-25 23:20:50 -0500

    feat: register embedding service in dependency injection container

--- src/core/di/container.ts
+++ src/core/di/container.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe'
 
-import { DATABASE_CONTEXT, UNIT_OF_WORK } from './tokens'
+import { DATABASE_CONTEXT, EMBEDDING_SERVICE, UNIT_OF_WORK } from './tokens'
 import { DatabaseContext, type IDatabaseContext } from '../database/database-context'
 import { UnitOfWork, type IUnitOfWork } from '../database/unit-of-work'
+import { EmbeddingService, type IEmbeddingService } from '../embedding/embedding.service'
 
 export const registerCoreDependencies = () => {
 	container.registerSingleton<IDatabaseContext>(DATABASE_CONTEXT, DatabaseContext)
 	container.registerSingleton<IUnitOfWork>(UNIT_OF_WORK, UnitOfWork)
+	container.registerSingleton<IEmbeddingService>(EMBEDDING_SERVICE, EmbeddingService)
 }
--- src/core/di/tokens.ts
+++ src/core/di/tokens.ts
@@ -1,2 +1,4 @@
 export const DATABASE_CONTEXT = Symbol.for('IDatabaseContext')
 export const UNIT_OF_WORK = Symbol.for('IUnitOfWork')
+
+export const EMBEDDING_SERVICE = Symbol.for('IEmbeddingService')
