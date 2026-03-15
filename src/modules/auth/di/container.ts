3458d0aaf5b975c162c9ffc6e88438324ae58104
Jose-Maykol <jpaniura@unsa.edu.pe>
2026-03-14 20:56:06 -0500

    feat(auth): register RefreshTokenRepository in DI container

--- src/modules/auth/di/container.ts
+++ src/modules/auth/di/container.ts
@@ -1,7 +1,14 @@
 import { container } from 'tsyringe'
 
-import { AUTH_CONTROLLER, AUTH_SERVICE, GOOGLE_AUTH_SERVICE, TOKEN_SERVICE } from './tokens'
+import {
+	AUTH_CONTROLLER,
+	AUTH_SERVICE,
+	GOOGLE_AUTH_SERVICE,
+	TOKEN_SERVICE,
+	REFRESH_TOKEN_REPOSITORY
+} from './tokens'
 import { AuthController } from '../controllers/auth.controller'
+import { RefreshTokenRepository } from '../repositories/refresh-token.repository'
 import { AuthService } from '../services/auth.service'
 import { GoogleAuthService } from '../services/google-auth.service'
 import { TokenService } from '../services/token.service'
@@ -11,4 +18,5 @@ export const registerAuthDependencies = () => {
 	container.register(AUTH_SERVICE, { useClass: AuthService })
 	container.register(AUTH_CONTROLLER, { useClass: AuthController })
 	container.register(TOKEN_SERVICE, { useClass: TokenService })
+	container.register(REFRESH_TOKEN_REPOSITORY, { useClass: RefreshTokenRepository })
 }
