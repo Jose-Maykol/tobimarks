2fbe7d58e7e89cce14045d35dc920b61ab1ea74c
Jose-Maykol <jpaniura@unsa.edu.pe>
2026-05-07 23:06:11 -0500

    refactor(collection): standardize exceptions using BaseException and add documentation

--- src/modules/collection/exceptions/collection.exceptions.ts
+++ src/modules/collection/exceptions/collection.exceptions.ts
@@ -1,13 +1,38 @@
-export class CollectionNotFoundError extends Error {
-	constructor(public readonly code: string = 'COLLECTION_NOT_FOUND') {
-		super('Collection not found')
-		this.name = 'CollectionNotFoundError'
+import { BaseException } from '@/common/errors/base-erxception'
+
+export enum CollectionErrorCode {
+	COLLECTION_NOT_FOUND = 'COLLECTION_NOT_FOUND',
+	COLLECTION_ALREADY_EXISTS = 'COLLECTION_ALREADY_EXISTS'
+}
+
+/**
+ * Excepción lanzada cuando no se encuentra una colección específica.
+ *
+ * @extends BaseException
+ * @example
+ * throw new CollectionNotFoundError('No se encontró la colección solicitada');
+ */
+export class CollectionNotFoundError extends BaseException {
+	/**
+	 * @param message - Mensaje descriptivo del error.
+	 */
+	constructor(message: string = 'Collection not found') {
+		super(message, CollectionErrorCode.COLLECTION_NOT_FOUND)
 	}
 }
 
-export class CollectionAlreadyExistsError extends Error {
-	constructor(public readonly code: string = 'COLLECTION_ALREADY_EXISTS') {
-		super('You already have a collection with this name')
-		this.name = 'CollectionAlreadyExistsError'
+/**
+ * Excepción lanzada cuando se intenta crear una colección con un nombre que ya está en uso.
+ *
+ * @extends BaseException
+ * @example
+ * throw new CollectionAlreadyExistsError('Ya tienes una colección con este nombre');
+ */
+export class CollectionAlreadyExistsError extends BaseException {
+	/**
+	 * @param message - Mensaje descriptivo del error.
+	 */
+	constructor(message: string = 'You already have a collection with this name') {
+		super(message, CollectionErrorCode.COLLECTION_ALREADY_EXISTS)
 	}
 }
